a4d44567f0b0f535b0592ab0bc5b6ae356611e03
Samuel <email@samuelparker.tech>
2024-07-30 13:01:48 +0100

    Delete eny.py

--- eny.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"]='postgres://ula6gj4habu:gh1em6ZRkCcW@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/spoof_liver_avert_987852'
-
-os.environ["SECRET_KEY"]='c2VjcmV0X2tleV9leGFtcGxlX3Bhc3N3b3Jk'
