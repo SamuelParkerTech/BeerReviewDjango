20bb0e8b2cd3414817d9eee1db80541df066ba28
Samuel Parker <email@samuelparker.tech>
2024-08-08 12:55:24 +0000

    views matched to post data and css change to review form

---
+++ beer_card/forms.py
@@ -0,0 +1,8 @@
+from .models import Review
+from django import forms
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('review_content',)
--- beer_card/views.py
+++ beer_card/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.views import generic
 from .models import Beer
+from .forms import ReviewForm
 
 class BeerList(generic.ListView):
     queryset = Beer.objects.all()
@@ -13,11 +14,21 @@ def beer_detail(request, slug):
     beer = get_object_or_404(queryset, slug=slug)
     reviews = beer.reviews.all().order_by("-created_on")
     reviews_count = beer.reviews.filter(approved=True).count()
+    if request.method == "POST": 
+        review_form = ReviewForm(data=request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.poster = request.user
+            review.beer_name = beer_name
+            review.save()
+    
+    review_form = ReviewForm()
 
     return render(
         request,
         "beer_detail.html",
         {"beer": beer,
         "reviews": reviews,
-        "reviews_count": reviews_count },
+        "reviews_count": reviews_count, 
+        "review_form": review_form},
     )
