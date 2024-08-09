d4e2b6038e9159531d1709520cf9451a1f3a9821
Samuel Parker <email@samuelparker.tech>
2024-08-09 11:46:25 +0000

    reviews are now linked and posting when subitted, message appears as well

--- beer_card/forms.py
+++ beer_card/forms.py
@@ -5,4 +5,8 @@ from django import forms
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ('review_title','review_content','poster','rating',)
+        fields = ('review_title','review_content','rating',)
+        widgets = {
+            'review_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your review title'}),
+            'review_content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your review here'}),
+            'rating': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 5}),}
--- beer_card/views.py
+++ beer_card/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
 from django.views import generic
 from .models import Beer
 from .forms import ReviewForm
+from django.contrib import messages
 
 class BeerList(generic.ListView):
     queryset = Beer.objects.all()
@@ -14,19 +15,26 @@ def beer_detail(request, slug):
     beer = get_object_or_404(queryset, slug=slug)
     reviews = beer.reviews.all().order_by("-created_on")
     reviews_count = beer.reviews.filter(approved=True).count()
+
     if request.method == "POST": 
         review_form = ReviewForm(data=request.POST)
         if review_form.is_valid():
             review = review_form.save(commit=False)
             review.poster = request.user
-            review.review_title = review_title
-            review.review_content = review_content
-            review.rating = rating
+            review.review_title = review_form.cleaned_data.get('review_title')
+            review.review_content = review_form.cleaned_data.get('review_content')
+            review.rating = review_form.cleaned_data.get('rating')
+            review.beer_name = beer  # Explicitly associate the review with the beer
             review.save()
             messages.add_message(
-                request, messages.SUCCESS,  'Review has been submitted for approval'
+                request, messages.SUCCESS,  'Review has been submitted'
             )
-    review_form = ReviewForm()
+
+            
+            return redirect('beer_detail', slug=slug)  # Redirect to the same page after submission
+
+    else:
+        review_form = ReviewForm()
 
     return render(
         request,
